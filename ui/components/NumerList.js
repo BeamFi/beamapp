c4dc6ea27a1ecb60f1f80b40a364b4712db8fd4d
Henry Chan <henry@kinwo.net>
2022-09-09 16:24:10 +1000

    [Cleanup]

--- ui/components/NumerList.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-import { OrderedList } from "@chakra-ui/react"
-
-export const NumberList = ({ children }) => {
-  return <OrderedList spacing="10px">{children}</OrderedList>
-}
